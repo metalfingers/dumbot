d978e7a6cd5431cb8fa1120e2be0798945416d96
Richard Lewis <richard.lewis@nurun.com>
2016-11-11 18:28:44 -0500

    add a bunch of pending tests

--- test/test.js
+++ test/test.js
@@ -1,20 +1,32 @@
 // Libs
 const chai = require('chai'),
-  expect = chai.expect,
-  should = chai.should;
+  { expect } = chai;
 
 // Source
 const Dumbot = require('../index.js'),
   intents = require('./dumbot-config/intents.js'),
   intentHandlers = require('./dumbot-config/intent-handlers.js'),
   path = require('path'),
-  conversationsPath = path.resolve('./app/dumbot-config/conversations/'),
-  dumbot = new Dumbot(intents, intentHandlers, conversationsPath),
-  MockUser = require('./test/utils/mock-user.js');
+  conversationsPath = path.resolve('./test/dumbot-config/conversations/'),
+  dumbot = new Dumbot({ intents, intentHandlers, conversationsPath }),
+  MockUser = require('./mocks/mock-user.js');
 
-/**
- * To test first exchange
- */
-describe('Conversation - First exchange "Get Started" Test', () => {
 
+describe('new Dumbot', () => {
+  it('should error out if an object is not passed in');
+  it('should error out if no intents are passed in');
+  it('should console.warn if no intent handlers are passed in');
+  it('should error out if no conversations path is passed in');
+});
+
+describe('dumbot#handleMessage', () => {
+  it('should error out if an object is not passed in');
+  it('should be a function');
+  it('should error out if no user is passed in');
+  it('should error out if no message is undefined');
+});
+
+
+describe('dumbot#conversations', () => {
+  it('should exist');
 });
